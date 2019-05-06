ce5b697c088dd4e00a8d1734a068fa2005a31abb
Bosco <bkwan15@my.bcit.ca>
2019-05-06 14:57:28 -0700

    changed the music file

--- engine/junkdunker/Level1.js
+++ engine/junkdunker/Level1.js
@@ -3,6 +3,11 @@ var image2;
 var image3;
 var image4;
 var image5;
+var whaleImg;
+var groundImg;
+var trashImg;
+var music;
+var platforms;
 class Level1 extends Phaser.Scene {
     constructor() {
         super({
@@ -10,21 +15,45 @@ class Level1 extends Phaser.Scene {
         });
     }
     preload() {
+        this.load.audio('bgmusic',['audio/music.mp3']);
+        this.load.image('ground', 'images/platform.png');
+        this.load.image('trash','images/trash.png');
         this.load.image('bin','images/bin.png');
+        this.load.image('whale', 'images/whale.jpg');
     }
 
     create() {
         this.cameras.main.setBackgroundColor('#00BFFF');
-        image1 = this.add.image(100,400,'bin');
-        image1.setScale(0.3);
-        image2 = this.add.image(250,400,'bin');
-        image2.setScale(0.3);
-        image3 = this.add.image(400,400,'bin');
-        image3.setScale(0.3);
-        image4 = this.add.image(550,400,'bin');
-        image4.setScale(0.3);
-        image5 = this.add.image(700,400,'bin');
-        image5.setScale(0.3);
+        // creates the music
+        this.bgmusic = this.sound.add('bgmusic');
+        this.bgmusic.play();
+
+        // Creates the images on the page
+        image1 = this.add.image(100,450,'bin');
+        image1.setScale(0.2);
+        image2 = this.add.image(250,450,'bin');
+        image2.setScale(0.2);
+        image3 = this.add.image(400,450,'bin');
+        image3.setScale(0.2);
+        image4 = this.add.image(550,450,'bin');
+        image4.setScale(0.2);
+        image5 = this.add.image(700,450,'bin');
+        image5.setScale(0.2);
+        groundImg = this.add.image(600,100,'ground');
+        groundImg.setScale(0.4);
+
+
+
+        // Creates the falling trash on the page
+        trashImg = this.physics.add.sprite(400,50,'trash');
+        trashImg.setScale(0.5)
+        trashImg.body.setGravity(300);
+        this.trashImg.setCollideWorldBounds(true);
+
+        whaleImg = this.add.image(800,450,'whale');
+        whaleImg.setScale(0.3);
+        //Adds the collider for the objects
+        this.physics.add.collider(this.ground,this.trash);
 
 
     }
