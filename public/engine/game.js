2466db72dadcf40612abd52206722814ae1cdb74
Nomad <pamirkantar21@gmail.com>
2019-05-07 09:35:12 -0700

    firebase config of game

---
+++ public/engine/game.js
@@ -0,0 +1,23 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 500,
+    scale: {
+        scale: 'SHOW_ALL',
+        orientation: 'LANDSCAPE',
+    },
+    resolution: window.devicePixelRatio,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: {
+                y: 500
+            }
+        }
+    },
+    scene: [Intro,Level1]
+};
+
+var game = new Phaser.Game(config);
