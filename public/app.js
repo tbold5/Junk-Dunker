3afce2ff849bbc164ccd380028e640958601ea64
craiggoodison <cgoodison1@my.bcit.ca>
2019-05-08 11:25:08 -0700

    'Test'

--- public/app.js
+++ public/app.js
@@ -2,3 +2,16 @@ document.addEventListener('DOMContentLoaded', event => {
     const app = firebase.app();
 });
 
+document.getElementsByClassName("nes-btn is-success").addEventListener("click", displayPlay);
+//document.getElementById("nes-btn is-success").addEventListener("click", );
+document.getElementsByClassName("nes-btn is-warning").addEventListener("click", displayLeaderboard);
+document.getElementsByClassName("nes-btn is-error").addEventListener("click", displaySettings);
+
+function displayPlay() {
+  document.getElementsByClassName("nes-btn is-success").innerHTML = Date();}
+
+function displayLeaderboard() {
+  document.getElementsByClassName("nes-btn is-warning").innerHTML = Date();}
+
+function displaySettings() {
+  document.getElementsByClassName("nes-btn is-error").innerHTML = Date();}
