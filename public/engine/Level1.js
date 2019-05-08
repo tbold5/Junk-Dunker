3646600dd96ad53ef48df7ae3c70ecf4ffa96756
Bosco <bkwan15@my.bcit.ca>
2019-05-08 16:24:36 -0700

    added background image

--- public/engine/Level1.js
+++ public/engine/Level1.js
@@ -1,11 +1,13 @@
-var image1;
-var image2;
-var image3;
-var image4;
-var image5;
+var blueBin;
+var redBin;
+var greenBin;
+var yellowBin;
+var greyBin;
 var bagImg;
 var groundImg;
 var trashImg;
+var boardBoxImg;
+var detergentImg;
 var music;
 var platforms;
 var gameWidth;
@@ -22,13 +24,29 @@ class Level1 extends Phaser.Scene {
         });
     }
     preload() {
+
+        // Loads all the audio files
         this.load.audio('bgmusic',['audio/music.mp3']);
-        this.load.image('ground', 'images/platform.png');
-        this.load.image('trash','images/trash.png');
-        this.load.image('bin','images/bin.png');
-        this.load.image('bags', 'images/bags.png');
-        this.load.image('beerBottle','images/beerbottle.png');
         this.load.audio('pop',['audio/pop.mp3']);
+
+        // loads all the recycling bins
+        this.load.image('greyBin','images/greybin1.png');
+        this.load.image('blueBin','images/bluebin1.png');
+        this.load.image('yellowBin','images/yellowbin1.png');
+        this.load.image('redBin','images/redbin1.png');
+        this.load.image('greenBin','images/greenbin1.png');
+
+        this.load.image('ground', 'images/platform.png');
+        this.load.image('bg', 'images/backGround.gif');
+
+        // loads all the garbage
+        this.load.image('bags', 'images/bags.png');
+        this.load.image('detergent', 'images/detergent.png');
+        this.load.image('beerBottle','images/beerbottle.png');
+        this.load.image('boardBox', 'images/cardboardbox.png');
+        this.load.image('trash','images/trash.png');
+
+
     }
 
 
@@ -46,31 +64,39 @@ class Level1 extends Phaser.Scene {
     create() {
         gameWidth = game.config.width;
         gameHeight = game.config.height;
-        this.cameras.main.setBackgroundColor('#00BFFF');
+        // this.cameras.main.setBackgroundColor('#00BFFF');
         // creates the music
         // this.bgmusic = this.sound.add('bgmusic');
         // this.bgmusic.play();
 
+        // Creates the background image
+        this.backGround = this.add.image(gameWidth/2,gameHeight/2,'bg');
+        this.backGround.setDisplaySize(gameWidth,gameHeight);
+
+
         popSound = this.pop = this.sound.add('pop');
         // this.pop.play();
         // Creates the images on the page
-        image1 = this.add.image(gameWidth / 8, gameHeight - 50,'bin');
-        image1.setScale(0.1);
-        image2 = this.add.image(gameWidth / 2.7, gameHeight - 50,'bin');
-        image2.setScale(0.1);
-        image3 = this.add.image(gameWidth / 1.6, gameHeight - 50,'bin');
-        image3.setScale(0.1);
-        image4 = this.add.image(gameWidth / 1.14, gameHeight - 50,'bin');
-        image4.setScale(0.1);
+        redBin = this.add.image(gameWidth / 8, gameHeight - 50,'redBin');
+        redBin.setScale(0.05);
+        yellowBin = this.add.image(gameWidth / 2.7, gameHeight - 50,'yellowBin');
+        yellowBin.setScale(0.05);
+        greyBin = this.add.image(gameWidth / 1.6, gameHeight - 50,'greyBin');
+        greyBin.setScale(0.05);
+        blueBin = this.add.image(gameWidth / 1.14, gameHeight - 50,'blueBin');
+        blueBin.setScale(0.05);
         groundImg = this.add.image(600,100,'ground');
         groundImg.setScale(1);
 
-        console.log('gameHeight:' , gameHeight);
-        console.log('gameWidth:', gameWidth);
 
         // Creates the middle bin
-        image5 = this.physics.add.sprite(gameWidth / 2, gameHeight / 1.5,'bin');
-        image5.setScale(0.3);
+        greenBin = this.physics.add.sprite(gameWidth / 2, gameHeight / 1.5,'greenBin');
+        greenBin.setScale(0.1);
+
+        // Creates cardboard box image
+        boardBoxImg = this.physics.add.sprite(gameWidth / 2, 50, 'boardBox');
+        boardBoxImg.setScale(0.5);
+        boardBoxImg.setGravity(0,50);
 
         // Creates the falling trash on the page
         trashImg = this.physics.add.sprite(gameWidth / 2, 50,'trash');
@@ -78,26 +104,34 @@ class Level1 extends Phaser.Scene {
         trashImg.setCollideWorldBounds(true);
         trashImg.setGravity(0,100);
 
-        // Creates bag image
+        // Creates bag sprite
         bagImg = this.physics.add.sprite(gameWidth / 2, 50,'bags');
         bagImg.setScale(0.9);
         bagImg.setCollideWorldBounds(true);
-        bagImg.setGravity(0,100);
+        bagImg.setGravity(0,150);
 
+        // Creates detergent sprite
+        detergentImg = this.physics.add.sprite(gameWidth / 2, 50,'detergent');
+        detergentImg.setScale(0.9);
+        detergentImg.setCollideWorldBounds(true);
+        detergentImg.setGravity(0,200);
 
         //Create beer bottle sprite
         beerBottle = this.physics.add.sprite(gameWidth / 2, 50,'beerBottle');
         beerBottle.setScale(0.9);
         beerBottle.setCollideWorldBounds(true);
 
+
         // this.aGrid = new AlignGrid({scene:this,rows:11,cols:11});
         // this.aGrid.showNumbers();
         //Adds the collider for the objects
 
-        this.physics.add.overlap(image5, trashImg,this.collectGarbage, null, this);
-        this.physics.add.overlap(image5, bagImg,this.collectGarbage, null, this);
+
 
     }
 
-    update(time, delta) {}
+    update(time, delta) {
+        this.physics.add.overlap(greenBin, trashImg,this.collectGarbage, null, this);
+        this.physics.add.overlap(greenBin, bagImg,this.collectGarbage, null, this);
+    }
 }
