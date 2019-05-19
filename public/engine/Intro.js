6c8343a027f783b56040480c8611ef32b60bb7ce
Bosco <bkwan15@my.bcit.ca>
2019-05-18 23:13:12 -0700

    fixed bug where when you load level 1 it doesn't take the background image from intro page

--- public/engine/Intro.js
+++ public/engine/Intro.js
@@ -9,14 +9,16 @@ class Intro extends Phaser.Scene {
         });
     }
     preload() {
+
         // loads the background image
-        this.load.image('bg', 'images/comicDONE.png');
+        this.load.image('comicDone', 'images/comicDONE.png');
+
 
         // loads the speech bubble which allows user to exit the game
-        this.load.image('exitBtn', 'images/nah.gif');
+        this.load.image('nahBtn', 'images/nah.gif');
 
         // loads the speech bubble which allows user to play the game
-        this.load.image('playBtn', 'images/yesBubble.gif');
+        this.load.image('yesBtn', 'images/yesBubble.gif');
 
 
 
@@ -24,26 +26,22 @@ class Intro extends Phaser.Scene {
     }
     create() {
         var nahBtn;
-        var playBtn;
+        var yesBtn;
+        var backGround;
 
 
         gameWidth = game.config.width;
         gameHeight = game.config.height;
 
-        backGroundImg = this.add.image(gameWidth / 2, gameHeight / 2, 'bg');
-        backGroundImg.setDisplaySize(gameWidth, gameHeight);
+        backGround = this.add.image(gameWidth / 2, gameHeight / 2, 'comicDone');
+        backGround.setDisplaySize(gameWidth, gameHeight);
 
 
-        playBtn = this.physics.add.sprite(gameWidth / 1.5, gameHeight / 1.3, 'playBtn');
-        playBtn.setScale(0.3);
-
-        playBtn.setInteractive({useHandCursor: true})
-            .on('pointerdown', () => {
-                this.scene.start('Level1');
-            }, this);
+        yesBtn = this.physics.add.sprite(gameWidth / 1.5, gameHeight / 1.3, 'yesBtn');
+        yesBtn.setScale(0.3);
 
 
-        nahBtn = this.physics.add.sprite(gameWidth / 1.25, gameHeight / 1.2, 'exitBtn');
+        nahBtn = this.physics.add.sprite(gameWidth / 1.25, gameHeight / 1.2, 'nahBtn');
         nahBtn.setScale(0.4);
 
         nahBtn.setInteractive({useHandCursor: true})
@@ -51,12 +49,18 @@ class Intro extends Phaser.Scene {
                 window.location.href = '../index.html';
             }, this);
 
+        yesBtn.setInteractive({useHandCursor: true})
+            .on('pointerdown', () => {
+                this.scene.start('Level1');
+            }, this);
+
+
 
 
     }
 
 
     update() {
-        // this.scene.launch('Level1');
+
     }
 }
